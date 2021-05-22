8e60bb592aebd16cf49ee060fb938601e39c8303
Mahmudul Hasan Hasib <hasib.mahmudul@gmail.com>
2021-05-22 19:47:47 +0600

    create post with factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(10)->create();
         \App\Models\Category::factory(15)->create();
+        \App\Models\Post::factory(100)->create();
     }
 }
